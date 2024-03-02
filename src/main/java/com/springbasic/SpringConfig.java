b55f58ffe9d0e7acbca952e878c53cd9edc1dee9
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-02 22:36:29 +0900

    refactor SpringConfig클래스 memberRepository메소드 반환값을 JpaMemberRepository로 수정

--- src/main/java/com/springbasic/SpringConfig.java
+++ src/main/java/com/springbasic/SpringConfig.java
@@ -1,23 +1,19 @@
 package com.springbasic;
 
-import com.springbasic.repository.JdbcMemberRepository;
-import com.springbasic.repository.JdbcTemplateMemberRepository;
-import com.springbasic.repository.MemberRepository;
-import com.springbasic.repository.MemoryMemberRepository;
+import com.springbasic.repository.*;
 import com.springbasic.service.MemberService;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
-    private DataSource dataSource;
+   private EntityManager em;
 
-    @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+   @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -29,6 +25,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
