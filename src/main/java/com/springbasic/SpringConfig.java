4db928a531620ad505dd033c966438a1e1be6266
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-02 23:36:13 +0900

    refactor: SpringConfig클래스에서 memberService메소드 제거

--- src/main/java/com/springbasic/SpringConfig.java
+++ src/main/java/com/springbasic/SpringConfig.java
@@ -2,30 +2,21 @@ package com.springbasic;
 
 import com.springbasic.repository.*;
 import com.springbasic.service.MemberService;
-import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
-   private EntityManager em;
+   private final MemberRepository memberRepository;
 
    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
-//        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
+        return new MemberService(memberRepository);
     }
 }
