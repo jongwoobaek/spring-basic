6c84c3333d461bd371143b25d01ffbb532bcc747
Jongwoo Baek <qorwhddn1234@gmail.com>
2024-03-02 16:18:28 +0900

    feat: MemberController에 list메소드 구현

--- src/main/java/com/springbasic/controller/MemberController.java
+++ src/main/java/com/springbasic/controller/MemberController.java
@@ -4,8 +4,11 @@ import com.springbasic.domain.Member;
 import com.springbasic.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -30,5 +33,14 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> memberList = memberService.findMembers();
+
+        model.addAttribute("members", memberList);
+
+        return "members/memberList";
     }
 }
